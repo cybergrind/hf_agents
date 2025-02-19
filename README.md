557e1cbce6f4b559c210b0abccdde5ae58ddfe41
Kirill Pinchuk <cybergrind@gmail.com>
2025-02-19 11:08:47 +0300

    first agent: initial version

--- README.md
+++ README.md
@@ -2,5 +2,9 @@ https://huggingface.co/learn/agents-course/
 
 
 ```bash
+# first agent
+uv run first_agent/first_agent.py
+
+# repl
 uv run ipython
 ```
---
+++ first_agent/first_agent.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+import sys
+sys.path.append('first_agent')
+
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel, load_tool, tool
+import datetime
+import requests
+import pytz
+import yaml
+from tools.final_answer import FinalAnswerTool
+from tools.visit_webpage import VisitWebpageTool
+from tools.web_search import DuckDuckGoSearchTool
+
+from Gradio_UI import GradioUI
+
+PROMPTS_FILE = Path(__file__).parent / "prompts.yaml"
+
+# Below is an example of a tool that does nothing. Amaze us with your creativity!
+@tool
+def my_custom_tool(arg1:str, arg2:int)-> str: # it's important to specify the return type
+    # Keep this format for the tool description / args description but feel free to modify the tool
+    """A tool that does nothing yet
+    Args:
+        arg1: the first argument
+        arg2: the second argument
+    """
+    return "What magic will you build ?"
+
+@tool
+def get_current_time_in_timezone(timezone: str) -> str:
+    """A tool that fetches the current local time in a specified timezone.
+    Args:
+        timezone: A string representing a valid timezone (e.g., 'America/New_York').
+    """
+    try:
+        # Create timezone object
+        tz = pytz.timezone(timezone)
+        # Get current time in that timezone
+        local_time = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+        return f"The current local time in {timezone} is: {local_time}"
+    except Exception as e:
+        return f"Error fetching time for timezone '{timezone}': {str(e)}"
+
+
+final_answer = FinalAnswerTool()
+visit_webpage = VisitWebpageTool()
+model = HfApiModel(
+    max_tokens=2096,
+    temperature=0.5,
+    model_id='Qwen/Qwen2.5-Coder-32B-Instruct',
+    custom_role_conversions=None,
+)
+
+
+# Import tool from Hub
+image_generation_tool = load_tool("agents-course/text-to-image", trust_remote_code=True)
+
+with PROMPTS_FILE.open('r') as stream:
+    prompt_templates = yaml.safe_load(stream)
+
+agent = CodeAgent(
+    model=model,
+    tools=[final_answer, visit_webpage,
+        DuckDuckGoSearchTool()], # add your tools here (don't remove final_answer)
+    max_steps=60,
+    verbosity_level=1,
+    grammar=None,
+    planning_interval=None,
+    name=None,
+    description=None,
+    prompt_templates=prompt_templates,
+    additional_authorized_imports=['requests', 're'],
+)
+
+
+GradioUI(agent).launch()
